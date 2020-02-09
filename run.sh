dee869f7d109e8bcc9e7a80869bb0c08da6a41ee
Xinyu Zhang <xinyuzhang@mail.nankai.edu.cn>
2020-02-10 01:51:01 +0800

    update

--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
 # Only label data (Baseline)
-CUDA_VISIBLE_DEVICES=0 python3 train_baseline.py --save_path "results/baseline"
+# CUDA_VISIBLE_DEVICES=0 python3 train_baseline.py --save_path "results/baseline"
 
 # Ada-Net
-CUDA_VISIBLE_DEVICES=0 python3 train_adanet.py --save_path "results/Ada-Net"
+# CUDA_VISIBLE_DEVICES=0 python3 train_adanet.py --save_path "results/ada-Net"
 
 # Meta-learning (Ours)
-CUDA_VISIBLE_DEVICES=0 python3 train_meta_learning.py --save_path "results/Meta-learning"
+CUDA_VISIBLE_DEVICES=0 python3 train_meta_learning.py --save_path "results/meta-learning" \
+                                                      --num_label "4000" \
+                                                      --epsilon "1e-2" \
+                                                      --multiplier "1." \
+                                                      --print_freq "1"
--- train_meta_learning.py
+++ train_meta_learning.py
@@ -20,13 +20,13 @@ parser.add_argument('--start_step', type=int, default=0, help='Start step (for r
 parser.add_argument('--batch_size', type=int, default=128, help='Batch size')
 parser.add_argument('--epsilon', type=float, default=1e-2, help='epsilon for estimation gradients')
 parser.add_argument('--lr', type=float, default=0.1, help='Initial learning rate')
-parser.add_argument('--lr_decay', type=float, default=0.1, help='Learning rate annealing multiplier')
+parser.add_argument('--gamma', type=float, default=0.1, help='Learning rate annealing multiplier')
 parser.add_argument('--multiplier', type=float, default=1., help='args.inner_lr=args.lr*args.multipler (for the label update)')
 parser.add_argument('--fix_inner', action='store_true', help='fix the inner learning rate')
 parser.add_argument('--type', default='0', type=str, choices=['0', '1', '2', '3'], help='normalization type of updated labels')
 parser.add_argument('--weight_decay', type=float, default=1e-4, help='Weight decay')
 parser.add_argument('--momentum', type=float, default=0.9, help='Momentum for SGD optimizer')
-parser.add_argument('--num_workers', type=int, default=8, help='Number of workers')
+parser.add_argument('--num_workers', type=int, default=4, help='Number of workers')
 parser.add_argument('--resume', type=str, default=None, help='Resume model from a checkpoint')
 # Misc
 parser.add_argument('--print_freq', type=int, default=1, help='Print and log frequency')
@@ -75,13 +75,13 @@ label_loader, unlabel_loader, test_loader = cifar10(
 
 # Build model
 logger.info("Building models...")
-model = FullModel().cuda()
+model = FullModel(stochastic=False).cuda()
 criterion = CrossEntropy().cuda()
 
 # Build optimizer and lr_scheduler
 logger.info("Building optimizer and lr_scheduler...")
 optimizer = SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
-lr_scheduler = MultiStepLR(optimizer, gamma=args.lr_decay,
+lr_scheduler = MultiStepLR(optimizer, gamma=args.gamma,
                            milestones=[args.total_steps//2, args.total_steps*3//4])
    
 # Optionally resume from a checkpoint
@@ -100,7 +100,6 @@ if args.resume is not None:
 def main():
     data_times, batch_times, label_losses, unlabel_losses, label_acc, unlabel_acc = [AverageMeter() for _ in range(6)]
     best_acc = 0.
-    model.train()
     logger.info("Start training...")
     for step in range(args.start_step, args.total_steps):
         # Load data and distribute to devices
@@ -124,6 +123,7 @@ def main():
         ### First-order Approximation ###
         _concat = lambda xs: torch.cat([x.view(-1) for x in xs])
         
+        model.eval()
         # Forward label data and perform backward pass
         label_pred = model(label_img)
         label_loss = F.cross_entropy(label_pred, label_gt, reduction='mean')
@@ -215,6 +215,7 @@ def main():
                 torch.relu_(unlabel_pseudo_gt)
         """
         
+        model.train()
         # Compute loss with `unlabel_pseudo_gt`
         unlabel_pred = model(unlabel_img)
         unlabel_loss = criterion(unlabel_pred, unlabel_grad_neg)
@@ -251,9 +252,7 @@ def main():
         unlabel_losses.update(unlabel_loss.item(), unlabel_img.size(0))
         label_acc.update(label_top1.item(), label_img.size(0))
         unlabel_acc.update(unlabel_top1.item(), unlabel_img.size(0))
-        
-        # Write to tfboard
-    
+            
         # Print and log
         if step % args.print_freq == 0:
             logger.info("Step: [{0:05d}/{1:05d}] Dtime: {dtimes.val:.3f} (avg {dtimes.avg:.3f}) "
@@ -268,7 +267,7 @@ def main():
 
         # Test and save model
         if (step + 1) % args.test_freq == 0 or step == args.total_steps - 1:
-            acc = test()
+            acc = evaluate()
             # remember best accuracy and save checkpoint
             is_best = acc > best_acc
             if is_best:
@@ -288,40 +287,41 @@ def main():
             writer.add_scalar('train/lr', optimizer.param_groups[0]['lr'], step)
             writer.add_scalar('test/accuracy', acc, step)
             # Reset the AverageMeters
-            label_losses, unlabel_losses, label_acc, unlabel_acc = [AverageMeter() for _ in range(4)]
+            label_losses.reset()
+            unlabel_losses.reset
+            label_acc.reset()
+            unlabel_acc.reset()
 
-
-def test():
+@torch.no_grad()
+def evaluate():
     batch_time, losses, acc = [AverageMeter() for _ in range(3)]
     # switch to evaluate mode
     model.eval()
 
-    with torch.no_grad():
+    end = time.time()
+    for i, (data, target) in enumerate(test_loader):
+        data = data.cuda()
+        target = target.cuda()
+
+        # compute output
+        pred = model(data)
+        loss = F.cross_entropy(pred, target, reduction='mean')
+        
+        # measure accuracy and record loss
+        top1, = accuracy(pred, target, topk=(1,))
+        losses.update(loss.item(), data.size(0))
+        acc.update(top1.item(), data.size(0))
+        # measure elapsed time
+        batch_time.update(time.time() - end)
         end = time.time()
-        for i, (data, target) in enumerate(test_loader):
-            data = data.cuda()
-            target = target.cuda()
+        if i % args.print_freq == 0:
+            logger.info('Test: [{0}/{1}] Time {btime.val:.3f} (avg={btime.avg:.3f}) '
+                        'Test Loss {loss.val:.3f} (avg={loss.avg:.3f}) '
+                        'Acc {acc.val:.3f} (avg={acc.avg:.3f})' \
+                        .format(i, len(test_loader), btime=batch_time, loss=losses, acc=acc))
 
-            # compute output
-            pred = model(data)
-            loss = F.cross_entropy(pred, target, reduction='mean')
-            
-            # measure accuracy and record loss
-            top1, = accuracy(pred, target, topk=(1,))
-            losses.update(loss.item(), data.size(0))
-            acc.update(top1.item(), data.size(0))
-            # measure elapsed time
-            batch_time.update(time.time() - end)
-            end = time.time()
-            if i % args.print_freq == 0:
-                logger.info('Test: [{0}/{1}] Time {btime.val:.3f} (avg={btime.avg:.3f}) '
-                            'Test Loss {loss.val:.3f} (avg={loss.avg:.3f}) '
-                            'Acc {acc.val:.3f} (avg={acc.avg:.3f})' \
-                            .format(i, len(test_loader), btime=batch_time, loss=losses, acc=acc))
-
-        logger.info(' * Accuracy {acc.avg:.5f}'.format(acc=acc))
+    logger.info(' * Accuracy {acc.avg:.5f}'.format(acc=acc))
     
-    model.train()
     return acc.avg
 
 # Train and evaluate the model
