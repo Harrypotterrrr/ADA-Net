b0ea4ae8e011221281efef376a8d0cc4874adeaf
KAI ZHAO <kaiz.xyz@gmail.com>
2020-03-25 00:06:25 +0800

    fix bugs in visualization

--- plot-features.py
+++ plot-features.py
@@ -1,4 +1,4 @@
-import argparse, torch
+import os, argparse, torch
 from os.path import  isfile, join
 import numpy as np
 import torch.nn as nn
@@ -72,13 +72,14 @@ parser.add_argument('--dataset', type=str, default='svhn', choices=['svhn', 'cif
 parser.add_argument('--data-path', type=str, default='./data', help='Data path')
 parser.add_argument('--checkpoint-path', type=str, help='Path to checkpoint file')
 parser.add_argument('--index-path', type=str, help='Path to indices file')
+parser.add_argument('--aligned-path', type=str, default=None, help='Path to file of aligned features')
 parser.add_argument('--save-path', type=str, help='Directory of save path')
 args = parser.parse_args()
 
 make_folder(args.save_path)
 
 with open(args.index_path, 'r') as f:
-    label_indices = [line.rstrip('\n') for line in f]
+    label_indices = [int(line.rstrip('\n')) for line in f]
 
 data_transform = transforms.Compose([
         transforms.ToTensor(),
@@ -90,98 +91,111 @@ elif args.dataset == "svhn":
     train_dataset = dsets.SVHN(root=args.data_path, split='train', download=True, transform=data_transform)
 
 unlabel_indices = []
-print("# data: %d" % len(train_dataset.data))
 for idx in range(len(train_dataset.data)):
-    unlabel_indices.append(idx)
+    if not idx in label_indices:
+        unlabel_indices.append(idx)
 
 label_num = len(label_indices)
 unlabel_num = len(unlabel_indices)
+print("# data: %d, labeled: %d, unlabeled: %d" % (len(train_dataset.data), label_num, unlabel_num))
 
-label_indices = SubsetRandomSampler(label_indices)
-unlabel_indices = SubsetRandomSampler(unlabel_indices)
-label_loader = DataLoader(train_dataset, batch_size=100, num_workers=4, sampler=label_indices)
-unlabel_loader = DataLoader(train_dataset, batch_size=100, num_workers=4, sampler=unlabel_indices)
-
-if args.dataset == "cifar10":
-    test_dataset = dsets.CIFAR10(root=args.data_path, train=False, download=True, transform=data_transform)
-elif args.dataset == "svhn":
-    test_dataset = dsets.SVHN(root=args.data_path, split='test', download=True, transform=data_transform)
+if args.aligned_path is not None and isfile(args.aligned_path):
+    embedded_features = np.load(join(args.save_path, "aligned.npy"))
+else:
+    label_indices = SubsetRandomSampler(label_indices)
+    unlabel_indices = SubsetRandomSampler(unlabel_indices)
+    label_loader = DataLoader(train_dataset, batch_size=100, num_workers=4, sampler=label_indices)
+    unlabel_loader = DataLoader(train_dataset, batch_size=100, num_workers=4, sampler=unlabel_indices)
     
-test_loader = DataLoader(test_dataset, batch_size=100, num_workers=4)
-
-test_num = len(test_loader.data)
-
-model = ConvLarge(num_classes=10).cuda()
-
-assert isfile(args.checkpoint_path), "No checkpoint at %s" % args.checkpoint_path
-checkpoint = torch.load(args.checkpoint_path)
-model.load_state_dict(checkpoint['model'])
-model.eval()
-
-label_features, unlabel_features, test_features = [], [], []
-label_categories, unlabel_categories, test_categories = [], [], []
-
-with torch.no_grad():
-    for data, labels in enumerate(label_loader):
-        data = data.cuda()
-        features = model(data)
+    if args.dataset == "cifar10":
+        test_dataset = dsets.CIFAR10(root=args.data_path, train=False, download=True, transform=data_transform)
+    elif args.dataset == "svhn":
+        test_dataset = dsets.SVHN(root=args.data_path, split='test', download=True, transform=data_transform)
         
-        label_features.append(features)
-        label_categories.append(labels)
+    test_loader = DataLoader(test_dataset, batch_size=100, num_workers=0)
+    
+    model = ConvLarge(num_classes=10).cuda()
+    
+    assert isfile(args.checkpoint_path), "No checkpoint at %s" % args.checkpoint_path
+    checkpoint = torch.load(args.checkpoint_path)
+    model.load_state_dict(checkpoint['model'])
+    model.eval()
+    
+    label_features, unlabel_features, test_features = [], [], []
+    label_categories, unlabel_categories, test_categories = [], [], []
+    
+    with torch.no_grad():
+        print("Processing labeled data...")
+        for step, (data, labels) in enumerate(label_loader):
+            data = data.cuda()
+            features = model(data)
+            
+            label_features.append(features)
+            label_categories.append(labels)
+    
+            if step % 100 == 0:
+                print("[%d/%d] Finished!" % (step, len(label_loader)))
 
-    for data, labels in enumerate(unlabel_loader):
-        data = data.cuda()
-        features = model(data)
-        
-        unlabel_features.append(features)
-        unlabel_categories.append(labels)
+        print("Processing unlabeled data...")
+        for step, (data, labels) in enumerate(unlabel_loader):
+            data = data.cuda()
+            features = model(data)
+            
+            unlabel_features.append(features)
+            unlabel_categories.append(labels)
+    
+            if step % 100 == 0:
+                print("[%d/%d] Finished!" % (step, len(unlabel_loader)))
 
-    for data, labels in enumerate(test_loader):
-        data = data.cuda()
-        features = model(data)
-        
-        test_features.append(features)
-        test_categories.append(labels)
+        print("Processing test data...")
+        for step, (data, labels) in enumerate(test_loader):
+            data = data.cuda()
+            features = model(data)
+            
+            test_features.append(features)
+            test_categories.append(labels)
+            
+            if step % 100 == 0:
+                print("[%d/%d] Finished!" % (step, len(test_loader)))
+    
+    label_features = torch.cat(label_features, dim=0).cpu().numpy()
+    unlabel_features = torch.cat(unlabel_features, dim=0).cpu().numpy()
+    test_features = torch.cat(test_features, dim=0).cpu().numpy()
+    
+    label_categories = torch.cat(label_categories, dim=0).cpu().numpy()
+    unlabel_categories = torch.cat(unlabel_categories, dim=0).cpu().numpy()
+    test_categories = torch.cat(test_categories, dim=0).cpu().numpy()
+    
+    all_features = np.concatenate([label_features, unlabel_features, test_features], axis=0)
+    print("Computing TSNE features...")
+    embedded_features = TSNE(n_components=2).fit_transform(all_features)
+    np.save(join(args.save_path, "aligned.npy"), embedded_features)
+    print("Done!")
 
-label_features = torch.cat(label_features, dim=0).cpu().numpy()
-unlabel_features = torch.cat(unlabel_features, dim=0).cpu().numpy()
-test_features = torch.cat(test_features, dim=0).cpu().numpy()
+fig, axes = plt.subplots(1, 4, figsize=(24, 6))
 
-label_categories = torch.cat(label_categories, dim=0).cpu().numpy()
-unlabel_categories = torch.cat(unlabel_categories, dim=0).cpu().numpy()
-test_categories = torch.cat(test_categories, dim=0).cpu().numpy()
+axes[0].scatter(embedded_features[:label_num, 0], embedded_features[:label_num, 1], c="red", cmap=plt.cm.Spectral, s=10)
+axes[0].set_xticks([])
+axes[0].set_yticks([])
 
-all_features = np.concatenate([label_features, unlabel_features, test_features], axis=0)
-embedded_features = TSNE(n_components=2).fit_transform(all_features)
-np.save(join(args.save_path, "aligned.npy"), embedded_features)
+axes[1].scatter(embedded_features[label_num:label_num+unlabel_num, 0], embedded_features[label_num:label_num+unlabel_num, 1], c="blue", cmap=plt.cm.Spectral, s=10)
+axes[1].set_xticks([])
+axes[1].set_yticks([])
 
+axes[2].scatter(embedded_features[label_num+unlabel_num:, 0], embedded_features[label_num+unlabel_num:, 1], c="grey", cmap=plt.cm.Spectral, s=10)
+axes[2].set_xticks([])
+axes[2].set_yticks([])
 
-plt.scatter(embedded_features[:label_num, 0], embedded_features[:label_num, 1], c="red", cmap=plt.cm.Spectral, s=10)
-axes = plt.gca()
-axes.set_xticks([])
-axes.set_yticks([])
-plt.savefig(join(args.save_path, "aligned_label.png"))
-plt.close()
+axes[3].scatter(embedded_features[label_num:label_num+unlabel_num, 0], embedded_features[label_num:label_num+unlabel_num, 1], c="blue", cmap=plt.cm.Spectral, s=10)
+axes[3].scatter(embedded_features[label_num+unlabel_num:, 0], embedded_features[label_num+unlabel_num:, 1], c="grey", cmap=plt.cm.Spectral, s=10)
+axes[3].scatter(embedded_features[:label_num, 0], embedded_features[:label_num, 1], c="red", cmap=plt.cm.Spectral, s=10)
+axes[3].set_xticks([])
+axes[3].set_yticks([])
 
-plt.scatter(embedded_features[label_num:label_num+unlabel_num, 0], embedded_features[label_num:label_num+unlabel_num, 1], c="blue", cmap=plt.cm.Spectral, s=10)
-axes = plt.gca()
-axes.set_xticks([])
-axes.set_yticks([])
-plt.savefig(join(args.save_path, "aligned_unlabel.png"))
-plt.close()
+filename = join(args.save_path, "visualization.pdf")
+plt.tight_layout()
+plt.savefig(filename)
+plt.close(fig)
 
-plt.scatter(embedded_features[label_num+unlabel_num:, 0], embedded_features[label_num+unlabel_num:, 1], c="grey", cmap=plt.cm.Spectral, s=10)
-axes = plt.gca()
-axes.set_xticks([])
-axes.set_yticks([])
-plt.savefig(join(args.save_path, "aligned_test.png"))
-plt.close()
+# os.system("pdfcrop --margins \"0 0 0 0\" %s %s"%(filename,filename))
 
-plt.scatter(embedded_features[:label_num, 0], embedded_features[:label_num, 1], c="red", cmap=plt.cm.Spectral, s=10)
-plt.scatter(embedded_features[label_num:label_num+unlabel_num, 0], embedded_features[label_num:label_num+unlabel_num, 1], c="blue", cmap=plt.cm.Spectral, s=10)
-plt.scatter(embedded_features[label_num+unlabel_num:, 0], embedded_features[label_num+unlabel_num:, 1], c="grey", cmap=plt.cm.Spectral, s=10)
-axes = plt.gca()
-axes.set_xticks([])
-axes.set_yticks([])
-plt.savefig(join(args.save_path, "aligned_all.png"))
-plt.close()
--- visualize.sh
+++ visualize.sh
@@ -1,5 +1,6 @@
-CUDA_VISIBLE_DEVICES="0" python3 plot-features.py \
+CUDA_VISIBLE_DEVICES="1" python3 plot-features.py \
         --dataset "svhn" \
         --checkpoint-path "results/svhn-labels1000-mile30-35-mixup0.1-wd5e-5-balanced-run2/model_best.pth" \
         --index-path "results/svhn-labels1000-mile30-35-mixup0.1-wd5e-5-balanced-run2/label_indices.txt" \
-        --save-path "results/svhn-labels1000-mile30-35-mixup0.1-wd5e-5-balanced-run2/visualization"
+        --save-path "results/svhn-labels1000-mile30-35-mixup0.1-wd5e-5-balanced-run2/visualization" \
+        --aligned-path "results/svhn-labels1000-mile30-35-mixup0.1-wd5e-5-balanced-run2/visualization/aligned.npy"
